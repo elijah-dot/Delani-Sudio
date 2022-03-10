21109b0a19c1c714d9c66c4c235ef45987bb5339
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-10 08:45:56 +0300

    js:done with javascript for class boxtwo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//image one
+
 $("#topone").click(function () {
   $("#textone").css("display", "block");
   $("#topone").css("display", "none");
@@ -6,3 +8,16 @@ $("#textone").click(function(){
     $("#topone").css("display","block");
     $("#textone").css("display","none");
 });
+
+//image two
+
+$("#toptwo").click(function () {
+    $("#texttwo").css("display", "block");
+    $("#toptwo").css("display", "none");
+   });
+  $("#texttwo").click(function(){
+      $("#toptwo").css("display","block");
+      $("#texttwo").css("display","none");
+  });
+
+
