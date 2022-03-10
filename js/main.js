51db76edf8d5ce645d3ba5cd055b613452552929
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-10 13:10:34 +0300

    starting of js in portfolio section

--- js/main.js
+++ js/main.js
@@ -3,32 +3,33 @@
 $("#topone").click(function () {
   $("#textone").css("display", "block");
   $("#topone").css("display", "none");
- });
-$("#textone").click(function(){
-    $("#topone").css("display","block");
-    $("#textone").css("display","none");
+});
+$("#textone").click(function () {
+  $("#topone").css("display", "block");
+  $("#textone").css("display", "none");
 });
 
 //image two
 
 $("#toptwo").click(function () {
-    $("#texttwo").css("display", "block");
-    $("#toptwo").css("display", "none");
-   });
-  $("#texttwo").click(function(){
-      $("#toptwo").css("display","block");
-      $("#texttwo").css("display","none");
-  });
+  $("#texttwo").css("display", "block");
+  $("#toptwo").css("display", "none");
+});
+$("#texttwo").click(function () {
+  $("#toptwo").css("display", "block");
+  $("#texttwo").css("display", "none");
+});
 
 //image three
 
 $("#topthree").click(function () {
-    $("#textthree").css("display", "block");
-    $("#topthree").css("display", "none");
-   });
-  $("#textthree").click(function(){
-      $("#topthree").css("display","block");
-      $("#textthree").css("display","none");
-  });
+  $("#textthree").css("display", "block");
+  $("#topthree").css("display", "none");
+});
+$("#textthree").click(function () {
+  $("#topthree").css("display", "block");
+  $("#textthree").css("display", "none");
+});
 
+// Portfolio Section.
 
