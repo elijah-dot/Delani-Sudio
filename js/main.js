15394a1f9190160af015ae105efc67d093339eec
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-10 08:36:54 +0300

    js:done with javascript for class boxone

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-$("")
+$("#topone").click(function () {
+  $("#textone").css("display", "block");
+  $("#topone").css("display", "none");
+ });
+$("#textone").click(function(){
+    $("#topone").css("display","block");
+    $("#textone").css("display","none");
+});
